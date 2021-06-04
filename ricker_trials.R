b9195d208fccc42b10d9b572e76523c99fd632c0
lukewarkentin <luke.warkentin@gmail.com>
2021-06-04 11:06:32 -0700

    initial commit

---
+++ ricker_trials.R
@@ -0,0 +1,85 @@
+# Compare ricker curves and SMSY values
+
+library(ggplot2)
+library(purrr)
+
+# Make series of alpha and beta values
+#alphas <- 2:4
+alphas <- seq(1.5,4, length.out=10)
+#betas <- 1/(c(10000,20000,30000))
+betas <- seq(1/10000,1/100000,length.out=10)
+# make into a data frame
+df <- expand.grid(alphas, betas)
+names(df) <- c("alpha", "beta")
+# generate SMSY based on alpha and beta
+#df$SMSY = log(df$alpha)*(0.5-0.07*log(df$alpha))/df$beta # Hilborn and Walters equation to estimate SMSY
+
+# function to get SMSY and Sgen from alpha and beta
+get_SMSY_Sgen <- function(a, b, int_lower, int_upper) {
+  SMSY <- log(a)*(0.5-0.07*log(a))/b # Hilborn and Walters equation to estimate SMSY
+  fun_Sgen <- function(Sgen, a, b, SMSY) {Sgen* a * exp( - b*Sgen) - SMSY}
+  Sgen <- uniroot(fun, interval=c(int_lower, int_upper), a=a, b=b, SMSY=SMSY)$root
+  est  <- data.frame(SMSY, Sgen)
+  est
+}
+# get SMSY and Sgen in new data frame
+ndf <- pmap_dfr(list(df$alpha, df$beta, 0, 1/df$beta*2), get_SMSY_Sgen)
+# bind SMSY and Sgen to alpha and beta
+df1 <- cbind(df, ndf)
+# function to check whether Sgen is correct. Should be the spawner number 
+#   that gives recruits=SMSY in one generation, no fishing
+check_Sgen <- function(a,b,S) {
+  a*S*exp(-b*S)
+}
+# yes, correctly gets to SMSY
+cbind(check_Sgen(a=df1$alpha, b=df1$beta, S=df1$Sgen), df1$SMSY)
+
+# Plot ricker curves with Sgen and SMSY
+plot(type="n", bty="l", x=0, y=0, xlim=c(0,15000), ylim=c(0,25000)) 
+abline(a=0, b=1, lty=3, col="gray")
+#for(i in sample(1:nrow(df), size=5, replace=FALSE)) {
+for(i in 40:50) {
+  curve(df1$alpha[i]*x * exp(-df1$beta[i]*x), add=TRUE, col=i)
+  abline(v=df1$Sgen[i], col=i, lty=2)
+  abline(v=df1$SMSY[i], col=i)
+#  text(x=1000+i*10000,y=30000, col=i, label=df$beta[i])
+}
+
+# Plot how SMSY and Sgen change with alpha and beta
+# SMSY
+png("figures/fig_SMSY~alpha.png")
+ggplot(df1, aes(x=alpha, y=SMSY, colour=beta)) +
+  geom_point() +
+  scale_colour_viridis_c() +
+  theme_bw()
+dev.off()
+
+# Sgen
+png("figures/fig_Sgen~alpha.png")
+ggplot(df1, aes(x=alpha, y=Sgen, colour=beta)) +
+  geom_point()+
+  scale_colour_viridis_c() +
+  theme_bw()
+dev.off()
+
+# Both are very sensitive to beta. SMSY is more sensitive to alpha. Sgen not very sensitive to alpha.
+
+# All, alpha and beta axes
+png("figures/fig_beta~alpha.png")
+ggplot(df1, aes(x=alpha, y=beta, color=SMSY, size=Sgen)) +
+  geom_point()+
+  scale_colour_viridis_c() +
+  theme_bw()
+dev.off()
+
+# All, Sgen and SMSY axes
+png("figures/fig_Sgen~SMSY.png")
+ggplot(df1, aes(x=SMSY, y=Sgen, color=alpha, size=beta)) +
+  geom_point()+
+  scale_colour_viridis_c() +
+  theme_bw()
+dev.off()
+
+# SMSY and Sgen very sensitive to beta, alpha is secondary. 
+# For given value of beta, Sgen is highest at intermediate alpha. 
+
